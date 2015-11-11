9608d82dbe48c02abe3434a7933e7d6d58ef8309
Carlo Nicolini <carlo.nicolini@iit.it>
2015-11-11 18:08:24 +0100

    ...

--- generate_agreement.m
+++ generate_agreement.m
@@ -1,4 +1,19 @@
 function D = generate_agreement(W, n_reps_agreement, method)
+%GENERATE_AGREEMENT
+
+%GENERATE_AGREEMENT      Compute the agreement matrix for a given method
+%
+%   Inputs      W,  weighted or binary undirected network.
+%               n_reps_agreement, number of samples
+%               method, handle to a community detection method that returns
+%               a row vector of community membership
+%
+%   Outputs:    P, the agreement matrix, average number of times that
+%   vertex i and vertex j are clustered in the same community over all
+%   n_reps_agreeement
+%
+%   Carlo Nicolini, Istituto Italiano di Tecnologia (2015).
+
     ci = [];
     parfor i=1:n_reps_agreement
         memb = method(W);
