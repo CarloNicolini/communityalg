d80b6fa9a7c2cbdd6c8e97736898f96f9949beeb
Carlo Nicolini <carlo.nicolini@iit.it>
2016-03-15 12:45:38 +0100

    fixed ring_of_custom_cliques (was producing stars of cliques) and added start_of_custom_cliques

--- ring_of_custom_cliques.m
+++ ring_of_custom_cliques.m
@@ -8,11 +8,14 @@ for i=1:length(sizes)
     nodeBeg = cumsizes(i)-sizes(i)+1;
     nodeEnd = cumsizes(i);
     A(nodeBeg:nodeEnd,nodeBeg:nodeEnd)=1;
-    
-    % add the connecting edge
-    A(cumsizes,cumsizes)=1;
     memb(nodeBeg:nodeEnd)=i;
 end
 
+% Add the interconnecting links
+for i=1:length(sizes)-1
+    A(cumsizes(i), cumsizes(i)+1)=1;
+    A(cumsizes(i)+1, cumsizes(i))=1;
+end
+A(1,n)=1;
+A(n,1)=1;
 A(1:n+1:n^2)=0;
-
