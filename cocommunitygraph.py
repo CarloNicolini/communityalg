011044a6f5e6eb2eddd930223412e9017f494d6e
Carlo Nicolini <carlo.nicolini@iit.it>
2016-03-30 09:43:04 +0200

    ...

--- cocommunitygraph.py
+++ cocommunitygraph.py
@@ -61,25 +61,6 @@ def drawbipartite(A):
     plt.show()
 
 
-def perp( a ) :
-    b = empty_like(a)
-    b[0] = -a[1]
-    b[1] = a[0]
-    return b
-
-# line segment a given by endpoints a1, a2
-# line segment b given by endpoints b1, b2
-# return 
-def seg_intersect(a1,a2, b1,b2) :
-    da = a2-a1
-    db = b2-b1
-    dp = a1-b1
-    dap = perp(da)
-    denom = dot( dap, db)
-    num = dot( dap, dp )
-    return (num / denom.astype(float))*db + b1
-    
-
 def drawbipartite2(A):
     from matplotlib.path import Path
     # Create the nx bipartite graph
