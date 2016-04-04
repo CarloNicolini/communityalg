a6f3a5d8c94f5fc1cf3b4ce7f13b706f214cb021
Carlo Nicolini <carlo.nicolini@iit.it>
2016-04-04 11:14:50 +0200

    fixed vonneumann_entropy with correct matrix logarithm

---
+++ clique.m
@@ -0,0 +1,4 @@
+function A=clique(n)
+
+A=ones(n);
+A(1:n+1:n*n)=0;
--- vonneumann_entropy.m
+++ vonneumann_entropy.m
@@ -2,11 +2,14 @@ function S  = vonneumann_entropy(A)
 % Implementation from Structural reducibility of multilayer networks.
 % De Domenico, Manlio Nicosia, Vincenzo Arenas, Alexandre Latora, Vito 
 % Nature Communications 6, 6864, (2015)
-
 L = quantum_density(A);
-S = -trace(L.*log(L));
+% The Von Neumann entropy is -\Tr[ L \log(L) ] where the product is
+% intended to be a matrix product and the logarithm is intended to be a
+% matrix logarithm
+% S = -trace(L*logm(L));
 % Another way to compute it to observe that the Von Neumann Entropy of a
 % density matrix corresponds to the Shannon Entropy of its power spectrum
 lambda = eig(L); % compute the spectrum
-lambda(find(lambda<=0))=eps; % fix for numerical precision
-S2 = - sum(lambda.*log(lambda));
+% keep only positive eigenvalues
+lambda = lambda(lambda>0);
+S = - sum(lambda.*log(lambda));
