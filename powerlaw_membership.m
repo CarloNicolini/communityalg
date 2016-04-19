1b77e9de9681805c43754110754878e2aaccb9d1
Carlo Nicolini <carlo.nicolini@iit.it>
2016-04-19 09:57:29 +0200

    ...

---
+++ powerlaw_membership.m
@@ -0,0 +1,12 @@
+function memb=powerlaw_membership(nmin,nmax,tau,num_nodes)
+
+c = powerlaw_comm_sizes(nmin,nmax,tau,num_nodes);
+
+memb=zeros(1,num_nodes);
+cumsizes = cumsum(c);
+
+for i=1:length(c)
+    nodeBeg = cumsizes(i)-c(i)+1;
+    nodeEnd = cumsizes(i);
+    memb(nodeBeg:nodeEnd)=i;
+end
