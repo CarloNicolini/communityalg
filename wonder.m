d0e63aaf1e4c933e1f7389dedf21c72f438d2bb1
Carlo Nicolini <carlo.nicolini@iit.it>
2016-04-26 17:20:14 +0200

    added wonder

--- wonder.m
+++ wonder.m
@@ -46,9 +46,9 @@ end
 
 D = 0.0;
 if (q > 0.0 && p > 0.0)
-    D = D + q*log10(q/p);
+    D = D + q*log(q/p);
 end
 
 if (q < 1.0 && p < 1.0)
-    D = D + (1.0-q)*log10((1.0-q)/(1.0-p));
+    D = D + (1.0-q)*log((1.0-q)/(1.0-p));
 end
