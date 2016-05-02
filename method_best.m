46b1ce6c36610417ee15a08ac05f936cfc57a461
Carlo Nicolini <carlo.nicolini@iit.it>
2016-05-02 17:56:47 +0200

    ...

--- method_best.m
+++ method_best.m
@@ -2,7 +2,10 @@ function [best_membership,best_qual] = method_best(A, method, nreps)
 %METHOD_BEST Returns the best membership of a stochastic community detection method repeated a given number of times.
 % Inputs:        A is the binary or weighted adjacency matrix 
 %                method is a function handle to a community detection method.  It works with functions in this form [membership, quality] = method(adjacency)
-%                nreps: is the total number of repetition that method is run
+%                nreps: is the total number of times that `method` is run
+%
+% CommunityAlg toolbox: a toolbox for community detection utilities in Matlab/Octave
+% Carlo Nicolini, Istituto Italiano Di Tecnologia (2016)
 
 best_qual = -inf;
 best_membership = [];
