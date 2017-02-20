3defa4efe9271bbfa2738ec6640d0d89c9c7d9ae
Carlo Nicolini <carlo.nicolini@iit.it>
2017-02-20 23:07:24 +0100

    ...

--- quantum_density.m
+++
@@ -1,6 +0,0 @@
-function rho = quantum_density(A)
-% S. Braunstein, S. Ghosh, S. Severini, 
-% The laplacian of a graph as a density matrix: 
-% a basic combinatorial approach to separability of mixed states,
-% Ann. of Combinatorics, 10, no 3 (2006), 291-317.
-rho = graph_laplacian(A)/sum(sum(A));
