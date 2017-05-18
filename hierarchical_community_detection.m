714932c202fc2817a145ffe63b67df55391e9e7e
Carlo Nicolini <carlo.nicolini@iit.it>
2017-05-18 14:58:02 +0200

    wrote a first implementation of the hierarchical community detection

--- hierarchical_community_detection.m
+++ hierarchical_community_detection.m
@@ -1,15 +1,21 @@
-function [dendr,quals] = hierarchical_community_detection(A,method)
+function [all_levels_memb] = hierarchical_community_detection(A,method)
 
 N=length(A);
 all_levels_memb = zeros(1,N);
 
-while length(unique(all_levels_memb(end,:))) < N
-	all_levels_memb = [all_levels_memb; hierarchical_helper(A,all_levels_memb(end,:)];
+level = 0;
+condition=true;
+while condition
+	all_levels_memb = [all_levels_memb; hierarchical_helper(A,method,all_levels_memb(end,:)) + max(all_levels_memb(end,:))];
+	level = level+1;
+    if length(unique(all_levels_memb(end,:))) == length(unique(all_levels_memb(end-1,:))) % No further splitting can find communities
+        condition=false;
+    end
 end
 
-function memb = hierarchical_helper(A,pre_memb)
+function memb = hierarchical_helper(A,method,pre_memb)
 	memb = zeros(1,length(A));
 	for c=unique(pre_memb(:))'
 	    nodes = find(pre_memb==c);
-	    memb(nodes) = method(A(nodes,nodes)) + max(memb);
+	    memb(nodes) = method(A(nodes,nodes)) + max(memb(:));
 	end
