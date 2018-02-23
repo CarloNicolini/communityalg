332a05b1d12e08c3c164727d32b40c86b9b8780e
carlo <carlo.nicolini@iit.it>
2018-02-23 10:09:07 +0100

    added generate brainnetviewer utility funciton

---
+++ gen_bnv_pics.m
@@ -0,0 +1,9 @@
+function gen_bnv_pics(W, node_coords, node_labels, memb, prefix, folder, template_file, options_file )
+
+write_brainet_community([folder '/' prefix], node_coords, memb , ones(length(W),1), node_labels);
+um=unique(memb(:))';
+parfor i=1:length(um)
+    c=um(i);
+    BrainNet_MapCfg(template_file, [folder '/' prefix '_c' num2str(c) '.node'], options_file, [folder '/' prefix '_c' num2str(c) '.png'] );
+    close all
+end
