a8290c8382df2c8c08d15b5b7df9efbe39d55b47
carlo <carlo.nicolini@iit.it>
2018-02-23 10:22:24 +0100

    ...

--- gen_bnv_pics.m
+++ gen_bnv_pics.m
@@ -2,7 +2,7 @@ function gen_bnv_pics(W, node_coords, node_labels, memb, prefix, folder, templat
 
 write_brainet_community([folder '/' prefix], node_coords, memb , ones(length(W),1), node_labels);
 um=unique(memb(:))';
-parfor i=1:length(um)
+for i=1:length(um)
     c=um(i);
     BrainNet_MapCfg(template_file, [folder '/' prefix '_c' num2str(c) '.node'], options_file, [folder '/' prefix '_c' num2str(c) '.png'] );
     close all
